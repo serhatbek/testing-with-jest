79897807b091c5c6e279d66247863e7c14d270be
serhatbek <serhat.bekk@gmail.com>
2024-09-26 00:53:47 +0300

    ➕ add testing libraries

---
+++ src/utils.test.js
@@ -0,0 +1,13 @@
+import { describe, expect, it } from "vitest";
+
+describe("⭐ utils", () => {
+  it("check 1 equal 1", () => {
+    expect(1).toEqual(1);
+  });
+});
+
+describe("⭐ utils", () => {
+  it("check 2 equal 4", () => {
+    expect(2 * 2).toEqual(4);
+  });
+});
