a411d9facd1c4be5754a0776e5b8d1770316c177
serhatbek <serhat.bekk@gmail.com>
2024-09-26 21:43:28 +0300

    ✨ testing utility functions

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,13 +1,26 @@
 import { describe, expect, it } from "vitest";
+import { range } from "./utils";
 
-describe("⭐ utils", () => {
-  it("check 1 equal 1", () => {
-    expect(1).toEqual(1);
+// Testing utility functions
+
+describe("utils", () => {
+  describe("⭐ is equal", () => {
+    it("check 2 equal 4", () => {
+      expect(2 * 2).toEqual(4);
+    });
   });
-});
 
-describe("⭐ utils", () => {
-  it("check 2 equal 4", () => {
-    expect(2 * 2).toEqual(4);
+  describe("⭐ range", () => {
+    it("return correct results from 1 - 6", () => {
+      const result = range(1, 6);
+      expect(result).toEqual([1, 2, 3, 4, 5]);
+    });
+  });
+
+  describe("⭐ range", () => {
+    it("return correct results from 41 - 45", () => {
+      const result = range(41, 45);
+      expect(result).toEqual([41, 42, 43, 44]);
+    });
   });
 });
--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,6 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./tests/setup",
+    // include: ["**/?(*.)test.js?(x)"],
   },
 });
